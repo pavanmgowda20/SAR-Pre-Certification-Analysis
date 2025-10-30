de2482636faa8ed5340bdccca89f25c491fe8850
Nilesh Murthy <nileshmurthy03@gmail.com>
2025-10-30 05:40:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1451175473-297d1",
+  "appId": "1:702363604385:web:8e2c77ee8c65537333a7e0",
+  "apiKey": "AIzaSyAFc3UBXhdynMmRDcryb3CGvlbxnYtwXA4",
+  "authDomain": "studio-1451175473-297d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "702363604385"
+};
