046cac1fed526d19cbaefe00c7801436c2cf2623
Nilesh-HB <nileshmurthy03@gmail.com>
2025-11-06 22:18:33 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: 'AIzaSyAHbZgJMPOdFqsMzvnm9rnlDi4DstLHxvg', // Hardcoded key
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
