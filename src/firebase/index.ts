99b3cb6832b5be4c96ddaf6d1c8c6297906383ec
pavanmgowda20 <pavanmgowda20@gmail.com>
2026-05-21 01:23:56 +0530

    Fix Vercel Firebase build warning

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -14,15 +14,10 @@ export function initializeFirebase() {
     // without arguments.
     let firebaseApp;
     try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
       firebaseApp = initializeApp(firebaseConfig);
+    } catch (e) {
+      console.warn('Firebase initialization failed', e);
+      throw e;
     }
 
     return getSdks(firebaseApp);
